a37ca9bddcdbbee4c52961aac5c4a28f7f139d4e
Jérôme Steunou <g_rom_sempai@hotmail.com>
2013-08-16 16:46:08 +0200

    remove space before (

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,41 +1,41 @@
 
-(function (comimoc, angular) {
+(function(comimoc, angular) {
     
     console.log(comimoc.ARGS);
 
-    comimoc.factory('Comments', ['$resource', 'COMIMOC_CONFIG', function ($resource, COMIMOC_CONFIG) {
+    comimoc.factory('Comments', ['$resource', 'COMIMOC_CONFIG', function($resource, COMIMOC_CONFIG) {
         // expose `Comments` resource service
         
         // transform on query or get
         var transformResponse = function(data) {
             var jsonData = angular.fromJson(data);
-            return jsonData.comments || jsonData.comment;
+            return jsonData.comments;
         };
         return $resource(COMIMOC_CONFIG.RESOURCES_LOCATION,
                          {},
-                         {'query':  {method:'GET', isArray: true, transformResponse: transformResponse}});
+                         {'query':  {method:'GET',  isArray: true, transformResponse: transformResponse}});
         
     }]);
     
-    comimoc.controller('CommentCtrl', ['$scope', 'Comments', 'COMIMOC_CONFIG', function ($scope, Comments, COMIMOC_CONFIG) {
+    comimoc.controller('CommentCtrl', ['$scope', 'Comments', 'COMIMOC_CONFIG', function($scope, Comments, COMIMOC_CONFIG) {
         // handle getting and posting new comments
         
         
         // -- private --
         
-        var getPage = function () {
+        var getPage = function() {
             var page = window.location.pathname;
-            if (COMIMOC_CONFIG.ARGS) {
+            if(COMIMOC_CONFIG.ARGS) {
                 page += window.location.search;
             }
-            if (COMIMOC_CONFIG.HASH) {
+            if(COMIMOC_CONFIG.HASH) {
                 page += window.location.hash;
             }
             
             return page;
         };
         
-        var getWebsite = function () {
+        var getWebsite = function() {
             return COMIMOC_CONFIG.WEBSITE || '';
         };
         
@@ -45,12 +45,12 @@
         $scope.comments = Comments.query({website: getWebsite(), page: getPage()});
         $scope.comment = new Comments();
         
-        $scope.submit = function (valid) {
-            if (valid) {
+        $scope.submit = function(valid) {
+            if(valid) {
                 $scope.comment.website = getWebsite();
                 $scope.comment.page = getPage();
                 
-                $scope.comment.$save().then(function (comment) {
+                $scope.comment.$save().then(function(comment) {
                     $scope.comment = new Comments();
                     $scope.comments.push(comment);
                 });
