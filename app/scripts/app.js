587c953191305d6bd667ddefc09cdb1c933b6fe0
Jérôme Steunou <g_rom_sempai@hotmail.com>
2013-08-16 16:29:54 +0200

    add config and doc about config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,9 @@
 
-(function(comimoc, angular) {
+(function (comimoc, angular) {
+    
+    console.log(comimoc.ARGS);
 
-    comimoc.factory('Comments', ['$resource', 'COMIMOC_RESOURCES', function($resource, COMIMOC_RESOURCES) {
+    comimoc.factory('Comments', ['$resource', 'COMIMOC_CONFIG', function ($resource, COMIMOC_CONFIG) {
         // expose `Comments` resource service
         
         // transform on query or get
@@ -9,26 +11,50 @@
             var jsonData = angular.fromJson(data);
             return jsonData.comments || jsonData.comment;
         };
-        return $resource(COMIMOC_RESOURCES,
+        return $resource(COMIMOC_CONFIG.RESOURCES_LOCATION,
                          {},
                          {'query':  {method:'GET', isArray: true, transformResponse: transformResponse}});
         
     }]);
     
-    comimoc.controller('CommentCtrl', ['$scope', 'Comments', function ($scope, Comments) {
+    comimoc.controller('CommentCtrl', ['$scope', 'Comments', 'COMIMOC_CONFIG', function ($scope, Comments, COMIMOC_CONFIG) {
         // handle getting and posting new comments
         
-        $scope.comments = Comments.query({website: "pelicoms.dev", page: "dtc"});
+        
+        // -- private --
+        
+        var getPage = function () {
+            var page = window.location.pathname;
+            if (COMIMOC_CONFIG.ARGS) {
+                page += window.location.search;
+            }
+            if (COMIMOC_CONFIG.HASH) {
+                page += window.location.hash;
+            }
+            
+            return page;
+        };
+        
+        var getWebsite = function () {
+            return COMIMOC_CONFIG.WEBSITE || '';
+        };
+        
+        
+        // -- public --
+        
+        $scope.comments = Comments.query({website: getWebsite(), page: getPage()});
         $scope.comment = new Comments();
         
-        $scope.submit = function() {
-            $scope.comment.website = "pelicoms.dev";
-            $scope.comment.page = "dtc";
-            
-            $scope.comment.$save().then(function(comment) {
-                $scope.comment = new Comments();
-                $scope.comments.push(comment);
-            });
+        $scope.submit = function (valid) {
+            if (valid) {
+                $scope.comment.website = getWebsite();
+                $scope.comment.page = getPage();
+                
+                $scope.comment.$save().then(function (comment) {
+                    $scope.comment = new Comments();
+                    $scope.comments.push(comment);
+                });
+            }
         };
             
     }]);
